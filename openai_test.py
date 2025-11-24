cfe8ca33791aa6f7ebbbf5007bc4e0cd2b2aafaa
Idk507 <danushidk507@gmail.com>
2025-11-24 22:24:38 +0530

    openai_test.py

---
+++ openai_test.py
@@ -0,0 +1,31 @@
+import os
+from openai import AzureOpenAI
+
+
+
+client = AzureOpenAI(
+    api_version=api_version,
+    azure_endpoint=endpoint,
+    api_key=subscription_key,
+)
+
+response = client.chat.completions.create(
+    messages=[
+        {
+            "role": "system",
+            "content": "You are a helpful assistant.",
+        },
+        {
+            "role": "user",
+            "content": "I am going to Paris, what should I see?",
+        }
+    ],
+    max_completion_tokens=13107,
+    temperature=1.0,
+    top_p=1.0,
+    frequency_penalty=0.0,
+    presence_penalty=0.0,
+    model=deployment
+)
+
+print(response.choices[0].message.content)
